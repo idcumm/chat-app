70966f4a9f031e6d45fe710edaa72e84fc25f048
John Doe <devesaguillem@gmail.com>
2023-08-23 18:14:09 +0200

    update

--- tests/other_tests/bytes.py
+++
@@ -1,5 +0,0 @@
-while True:
-    hola_input = str(input("que edad tienes: "))
-    if hola_input > 100:
-        print("estas muerto")
-        break
--- tests/other_tests/cont_characters.py
+++
@@ -1,3 +0,0 @@
-list = [""]
-if list:
-    print(True)
